f75b9c08b56314069a66f02218a5bbdca6b3a6c5
dev-idblfs <developer.idblfs@gmail.com>
2019-07-26 21:24:58 +0530

    v12

--- index.py
+++ index.py
@@ -1,31 +1,32 @@
 import youtube_dl
-from flask import Flask, escape, url_for,request,json,jsonify
+from flask import Flask, escape, url_for, request, json, jsonify
 
 app = Flask(__name__)
-app.config['PYTHONATH']='bypass_app'
+app.config['PYTHONATH'] = 'bypass_app'
 
 @app.route('/')
 def index():
-    return 'index'
-
+    return jsonify({"Message":"Wellcomed BY IDFLFS"})
 
 @app.route('/youtube')
 def youtube():
     if request.args:
         url = request.args.get('url')
         ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
-        result = ydl.extract_info(
-            url,
-            download=False # We just want to extract the info
-        )
-        if 'entries' in result:
-            # Can be a playlist or a list of videos
-            video = result['entries'][0]
-        else:
-            # Just a video
-            video = result
-        
-        list=video['formats']
-        return jsonify(list)
+        try:
+            result = ydl.extract_info(
+                url,
+                download=False  # We just want to extract the info
+            )
+            if 'entries' in result:
+                # Can be a playlist or a list of videos
+                video = result['entries'][0]
+            else:
+                # Just a video
+                video = result
+                list = video['formats']
+                return jsonify(list)
+        except :
+            return jsonify({"Message":'Please Enter Valid URL'})
     else:
-        return "login"
+        return jsonify({"Message":"Please Enter Any URL"})
